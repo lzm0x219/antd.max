7d9894d28cfd8c4681c877c3290f71104bc44651
0x219 <lzm0x219@gmail.com>
2022-09-25 17:45:18 +0800

    [Feature] Add NotFound layout

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,17 +1,25 @@
 import React, { useContext } from "react";
-import { context } from "dumi/theme";
 import { type IRouteComponentProps } from "@umijs/types";
 import { Box } from "rc-basic";
+import { matchPath } from "dumi";
+import { context } from "dumi/theme";
 import Nav from "./components/Nav";
 import Sidebar from "./components/Sidebar";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import NotFound from "./components/NotFound";
 import "./styles/global.less";
 
 export type LayoutProps = IRouteComponentProps;
 
 export default function Layout({ children, location }: LayoutProps) {
-  const con = useContext(context);
+  const { routes } = useContext(context);
+
+  const match = routes.find((item) => matchPath(location.pathname, item));
+
+  if (!match) {
+    return <NotFound />;
+  }
 
   return (
     <Box as="main" className="AntdMaxLayout">
